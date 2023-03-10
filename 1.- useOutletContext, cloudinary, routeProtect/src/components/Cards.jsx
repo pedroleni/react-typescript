3b353419947c52fcbe51a4a3a21e911616985af4
pedroleni <pedroleridanieto@hotmail.com>
2023-03-10 17:02:34 +0100

    boton logout ok

--- 1.- useOutletContext, cloudinary, routeProtect/src/components/Cards.jsx
+++ 1.- useOutletContext, cloudinary, routeProtect/src/components/Cards.jsx
@@ -1,4 +1,4 @@
-import "./Card.css";
+import "./Cards.css";
 
 import React from "react";
 const Cards = ({ data }) => {
--- 1.- useOutletContext, cloudinary, routeProtect/src/components/Nav.jsx
+++ 1.- useOutletContext, cloudinary, routeProtect/src/components/Nav.jsx
@@ -18,12 +18,18 @@ const Nav = () => {
               <NavLink to="anime">ANIME</NavLink>
             </li>
             <div className="containerUser">
-              <li>
+              <li className="logout">
                 <NavLink to="/" onClick={() => logout()} className="logout">
-                  LOGOUT
+                  <img
+                    className="iconLogout"
+                    src="https://res.cloudinary.com/dq186ej4c/image/upload/v1678462138/logout_FILL0_wght400_GRAD0_opsz48_dpfnju.png"
+                    alt=""
+                  />
                 </NavLink>
               </li>
               <img className="avatar" src={localStorage.getItem("urlIMG")} alt="" />
+
+              <h2 className="titleUser">{`  Hi ${user}`}</h2>
             </div>
           </ul>
         </nav>
