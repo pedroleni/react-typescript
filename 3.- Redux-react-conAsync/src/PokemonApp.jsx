a953792c9054d4ae231c39a5a24e5c649dfdf8ce
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2023-03-25 00:01:10 +0100

    buttton previus

--- 3.- Redux-react-conAsync/src/PokemonApp.jsx
+++ 3.- Redux-react-conAsync/src/PokemonApp.jsx
@@ -1,39 +1,46 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getPokemons } from './store/slices/pokemon';
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getPokemons } from "./store/slices/pokemon";
 
 export const PokemonApp = () => {
-
   const dispatch = useDispatch();
-  const { isLoading, pokemons = [], page } = useSelector( state => state.pokemons );
-
+  const {
+    isLoading,
+    pokemons = [],
+    page,
+  } = useSelector((state) => state.pokemons);
 
   useEffect(() => {
-    dispatch( getPokemons() );    
-  }, [])
-  
+    dispatch(getPokemons());
+  }, []);
 
   return (
     <>
-        <h1>PokemonApp</h1>
-        <hr />
-        <span>Loading: { isLoading ? 'True': 'False' }</span>
+      <h1>PokemonApp</h1>
+      <hr />
+      <div className="gallery">
+        {isLoading ? (
+          <span>Loading</span>
+        ) : (
+          <ul>
+            {pokemons.map(({ name }) => (
+              <li key={name}>{name}</li>
+            ))}
+          </ul>
+        )}
+      </div>
 
-        <ul>
-          {
-            pokemons.map( ({ name }) => (
-              <li key={ name }>{ name }</li>
-            ))
-          }
-        </ul>
-
+      {page != 1 && (
         <button
-          disabled={ isLoading }
-          onClick={ () => dispatch( getPokemons(page) ) }
+          disabled={isLoading}
+          onClick={() => dispatch(getPokemons(page - 2))}
         >
-          Next
+          Previus
         </button>
+      )}
+      <button disabled={isLoading} onClick={() => dispatch(getPokemons(page))}>
+        Next
+      </button>
     </>
-  )
-}
+  );
+};
