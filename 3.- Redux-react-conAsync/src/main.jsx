fd4372969fd3a2bd9da5d574b84a5ae40211cde5
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2023-03-24 23:21:18 +0100

    Ejemplo para clase con los thunk

--- 3.- Redux-react-conAsync/src/main.jsx
+++ 3.- Redux-react-conAsync/src/main.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import App from "./App";
+
 import "./index.css";
-import { store } from "./store/store";
+import { PokemonApp } from "./PokemonApp";
+
+import { store } from "./store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PokemonApp />
     </Provider>
   </React.StrictMode>
 );
--- 3.- Redux-react-conAsync/src/store/store.js
+++ 3.- Redux-react-conAsync/src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { counterSlice } from "./slices/counter";
+import { pokemonSlice } from "./slices/pokemon";
 
 export const store = configureStore({
   reducer: {
-    counter: counterSlice.reducer,
+    pokemons: pokemonSlice.reducer,
   },
 });
